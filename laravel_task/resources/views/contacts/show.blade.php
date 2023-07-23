67942c00e0a1f5309ec4ed05de6bf06dfb7d74bb
akash-saji <akashsajixyz>
2023-07-23 20:54:05 +0530

    laarvel project completed

---
+++ laravel_task/resources/views/contacts/show.blade.php
@@ -0,0 +1,17 @@
+@extends('contacts.layout')
+@section('content') 
+<div class="card">
+  <div class="card-header">View Page</div>
+  <div class="card-body">
+        <div class="card-body">
+        <p class="card-title">first Name : {{ $contacts->first_name }}</h5>
+        <p class="card-text">last name : {{ $contacts->last_name }}</p>
+        <p class="card-text">Phone : {{ $contacts->mobile }}</p>
+        <p class="card-title">Email : {{ $contacts->Email }}</p>
+        <p class="card-text">Address : {{ $contacts->address }}</p>
+        <p class="card-text">branch : {{ $contacts->branch }}</p>
+        <p class="card-title">mobile : {{ $contacts->mobile }}</p>
+        <p class="card-text">subject : {{ $contacts->subject }}</p>
+  </div>
+  </div>
+</div>
